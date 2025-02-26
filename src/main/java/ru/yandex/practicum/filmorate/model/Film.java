595cad6481afc38e21aaeb77494b28aba7c075b8
n00bertb <53352025+n00bertb@users.noreply.github.com>
2025-02-26 18:55:12 +0300

    Initial commit

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,23 +1,12 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
-import java.time.LocalDate;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
  * Film.
  */
-@Data
+@Getter
+@Setter
 public class Film {
-    @EqualsAndHashCode.Include
-    private int id;
-    @EqualsAndHashCode.Exclude
-    private String name;
-    @EqualsAndHashCode.Exclude
-    private String description;
-    @EqualsAndHashCode.Exclude
-    private LocalDate releaseDate;
-    @EqualsAndHashCode.Exclude
-    private int duration;
 }
