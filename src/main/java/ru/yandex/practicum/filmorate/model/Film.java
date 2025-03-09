80a75241966637ba6a79e8b1cd18213b3e95e6bb
Артём Самуйлов <artemka.samuiloff@yandex.ru>
2025-03-09 13:19:48 +0300

    первая попытка сдать финальное задание 10го спринта

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,23 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    @EqualsAndHashCode.Include
+    private int id;
+    @EqualsAndHashCode.Exclude
+    private String name;
+    @EqualsAndHashCode.Exclude
+    private String description;
+    @EqualsAndHashCode.Exclude
+    private LocalDate releaseDate;
+    @EqualsAndHashCode.Exclude
+    private int duration;
 }
