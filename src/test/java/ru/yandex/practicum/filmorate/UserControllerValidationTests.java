8417ccfd31b813583c144e2c19aff2e103d20f99
Артём Самуйлов <artemka.samuiloff@yandex.ru>
2025-03-24 01:09:21 +0300

    правки в тестах

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerValidationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerValidationTests.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.controller.UserController;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.UserService;
 import ru.yandex.practicum.filmorate.service.UserServiceImpl;
